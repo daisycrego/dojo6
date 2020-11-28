bc614dd119b060585d716200801cea0ce02fd29e
daisycrego <daisycrego@gmail.com>
2020-11-28 14:34:40 -0500

    Experimenting with line charts and mixed bar charts in d3.

---
+++ myProject/Public/javascript/charts.js
@@ -0,0 +1,549 @@
+var data = [{
+    "date": "2016",
+    "total": 2319475,
+    "Alzheimer's disease": 116103,
+    "Cancer": 598038,
+    "Chronic lower respiratory diseases": 154596,
+    "Diabetes": 80058,
+    "Unintentional injuries": 161374,
+    "Heart disease": 635260,
+    "Influenza and pneumonia": 51537,
+    "Kidney disease": 50046,
+    "Stroke": 142142,
+    "Suicide": 44965
+
+}, {
+    "date": "2015",
+    "total": 2712630,
+    "Alzheimer's disease": 110561,
+    "Cancer": 595930,
+    "Chronic lower respiratory diseases": 155041,
+    "Diabetes": 79535,
+    "Unintentional injuries": 146571,
+    "Heart disease": 633842,
+    "Influenza and pneumonia": 57062,
+    "Kidney disease": 49959,
+    "Stroke": 140323,
+    "Suicide": 44193
+}, {
+    "date": "2014",
+    "total": 2626418,
+    "Alzheimer's disease": 93541,
+    "Cancer": 591700,
+    "Chronic lower respiratory diseases": 147101,
+    "Diabetes": 76488,
+    "Unintentional injuries": 135928,
+    "Heart disease": 614348,
+    "Influenza and pneumonia": 55227,
+    "Kidney disease": 48146,
+    "Stroke": 133103,
+    "Suicide": 42826
+}, {
+    "date": "2013",
+    "total": 2596993,
+    "Alzheimer's disease": 84767,
+    "Cancer": 584881,
+    "Chronic lower respiratory diseases": 149205,
+    "Diabetes": 75578,
+    "Unintentional injuries": 130557,
+    "Heart disease": 611105,
+    "Influenza and pneumonia": 56979,
+    "Kidney disease": 47112,
+    "Stroke": 128978,
+    "Suicide": 41149
+}, {
+    "date": "2012",
+    "total": 2543279,
+    "Alzheimer's disease": 83637,
+    "Cancer": 582623,
+    "Chronic lower respiratory diseases": 143489,
+    "Diabetes": 73932,
+    "Unintentional injuries": 127792,
+    "Heart disease": 599711,
+    "Influenza and pneumonia": 50636,
+    "Kidney disease": 45622,
+    "Stroke": 128546,
+    "Suicide": 40600
+}, {
+    "date": "2011",
+    "total": 2515458,
+    "Alzheimer's disease": 84974,
+    "Cancer": 576691,
+    "Chronic lower respiratory diseases": 142943,
+    "Diabetes": 73831,
+    "Unintentional injuries": 126438,
+    "Heart disease": 596577,
+    "Influenza and pneumonia": 53826,
+    "Kidney disease": 45591,
+    "Stroke": 128932,
+    "Suicide": 39518
+}, {
+    "date": "2010",
+    "total": 2468435,
+    "Alzheimer's disease": 83494,
+    "Cancer": 574743,
+    "Chronic lower respiratory diseases": 138080,
+    "Diabetes": 69071,
+    "Unintentional injuries": 120859,
+    "Heart disease": 597689,
+    "Influenza and pneumonia": 50097,
+    "Kidney disease": 50476,
+    "Stroke": 129476,
+    "Suicide": 38364
+}, {
+    "date": "2009",
+    "total": 2437163,
+    "Alzheimer's disease": 79003,
+    "Cancer": 567628,
+    "Chronic lower respiratory diseases": 137353,
+    "Diabetes": 68705,
+    "Unintentional injuries": 118021,
+    "Heart disease": 599413,
+    "Influenza and pneumonia": 53692,
+    "Kidney disease": 48935,
+    "Stroke": 128842,
+    "Suicide": 36909
+}, {
+    "date": "2008",
+    "total": 2471984,
+    "Alzheimer's disease": 82435,
+    "Cancer": 565469,
+    "Chronic lower respiratory diseases": 141090,
+    "Diabetes": 70553,
+    "Unintentional injuries": 121902,
+    "Heart disease": 616828,
+    "Influenza and pneumonia": 56284,
+    "Kidney disease": 48237,
+    "Stroke": 134148,
+    "Suicide": 36035
+}, {
+    "date": "2007",
+    "total": 2423712,
+    "Alzheimer's disease": 74632,
+    "Cancer": 562875,
+    "Chronic lower respiratory diseases": 127924,
+    "Diabetes": 71382,
+    "Unintentional injuries": 123706,
+    "Heart disease": 616067,
+    "Influenza and pneumonia": 52717,
+    "Kidney disease": 46448,
+    "Stroke": 135952,
+    "Suicide": 34598
+}, {
+    "date": "2006",
+    "total": 2426264,
+    "Alzheimer's disease": 72432,
+    "Cancer": 559888,
+    "Chronic lower respiratory diseases": 124583,
+    "Diabetes": 72449,
+    "Unintentional injuries": 121599,
+    "Heart disease": 631636,
+    "Influenza and pneumonia": 56326,
+    "Kidney disease": 45344,
+    "Stroke": 137119,
+    "Suicide": 33300
+}, {
+    "date": "2005",
+    "total": 2448017,
+    "Alzheimer's disease": 71599,
+    "Cancer": 559312,
+    "Chronic lower respiratory diseases": 130933,
+    "Diabetes": 75119,
+    "Unintentional injuries": 117809,
+    "Heart disease": 652091,
+    "Influenza and pneumonia": 63001,
+    "Kidney disease": 43901,
+    "Stroke": 143579,
+    "Suicide": 32637
+}, {
+    "date": "2004",
+    "total": 2397615,
+    "Alzheimer's disease": 65965,
+    "Cancer": 553888,
+    "Chronic lower respiratory diseases": 121987,
+    "Diabetes": 73138,
+    "Unintentional injuries": 112012,
+    "Heart disease": 652486,
+    "Influenza and pneumonia": 59664,
+    "Kidney disease": 42480,
+    "Stroke": 150074,
+    "Suicide": 32439
+}, {
+    "date": "2003",
+    "total": 2448288,
+    "Alzheimer's disease": 63457,
+    "Cancer": 556902,
+    "Chronic lower respiratory diseases": 126382,
+    "Diabetes": 74219,
+    "Unintentional injuries": 109277,
+    "Heart disease": 685089,
+    "Influenza and pneumonia": 65163,
+    "Kidney disease": 42453,
+    "Stroke": 157689,
+    "Suicide": 31484
+}, {
+    "date": "2002",
+    "total": 2443387,
+    "Alzheimer's disease": 58866,
+    "Cancer": 557271,
+    "Chronic lower respiratory diseases": 124816,
+    "Diabetes": 73249,
+    "Unintentional injuries": 106742,
+    "Heart disease": 696947,
+    "Influenza and pneumonia": 65681,
+    "Kidney disease": 40974,
+    "Stroke": 162672,
+    "Suicide": 31655
+}, {
+    "date": "2001",
+    "total": 2416425,
+    "Alzheimer's disease": 53852,
+    "Cancer": 553768,
+    "Chronic lower respiratory diseases": 123013,
+    "Diabetes": 71372,
+    "Unintentional injuries": 101537,
+    "Heart disease": 700142,
+    "Influenza and pneumonia": 62034,
+    "Kidney disease": 39480,
+    "Stroke": 163538,
+    "Suicide": 30622
+}, {
+    "date": "2000",
+    "total": 2403351,
+    "Alzheimer's disease": 49558,
+    "Cancer": 553091,
+    "Chronic lower respiratory diseases": 122009,
+    "Diabetes": 69301,
+    "Unintentional injuries": 97900,
+    "Heart disease": 710760,
+    "Influenza and pneumonia": 65313,
+    "Kidney disease": 37251,
+    "Stroke": 167661,
+    "Suicide": 29350
+}];
+var key = ["Alzheimer's disease", "Cancer", "Chronic lower respiratory diseases", "Diabetes", "Unintentional injuries", "Heart disease", "Influenza and pneumonia", "Kidney disease", "Stroke", "Suicide"];
+var initStackedBarChart = {
+    draw: function (config) {
+        me = this,
+            domEle = config.element,
+            stackKey = config.key,
+            data = config.data,
+            margin = {
+                top: 20,
+                right: 40,
+                bottom: 30,
+                left: 40
+            },
+            parseDate = d3.timeParse("%Y");
+
+        var legendRectSize = 17;
+        var legendSpacing = 4;
+
+        //making graph responsive
+        default_width = 800;
+        default_height = 500;
+        default_ratio = default_width / default_height;
+
+        // Determine current size, which determines vars
+        function set_size() {
+            current_width = window.innerWidth;
+            current_height = window.innerHeight;
+            current_ratio = current_width / current_height;
+            // Check if height is limiting factor
+            if (current_ratio > default_ratio) {
+                h = default_height;
+                w = default_width;
+                // Else width is limiting
+            } else {
+                w = current_width;
+                h = w / default_ratio;
+                legendSpacing = 2;
+                legendRectSize = 7;
+            }
+            // Set new width and height based on graph dimensions
+            width = w - margin.left - margin.right;
+            height = h - margin.top - margin.bottom;
+        };
+        set_size();
+        //end responsive graph code
+
+        xScale = d3.scaleLinear().rangeRound([0, width]),
+            yScale = d3.scaleBand().rangeRound([height, 0]).padding(0.1),
+            color = d3.scaleOrdinal(d3.schemeCategory20c),
+            xAxis = d3.axisBottom(xScale).ticks(5),
+            yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%Y")),
+            svg = d3.select("#" + domEle).append("svg")
+            .attr("width", 1000 + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var stack = d3.stack()
+            .keys(stackKey)
+            .order(d3.stackOrder)
+            .offset(d3.stackOffsetNone);
+
+        var layers = stack(data);
+
+        //sorts data by date- lowest to highest
+        data.sort(function (a, b) {
+            return a.date - b.date;
+        });
+        yScale.domain(data.map(function (d) {
+            return parseDate(d.date);
+        }));
+
+        //x max
+        xScale.domain([0, d3.max(layers[layers.length - 1], function (d) {
+            return 2500000;
+        })]).nice();
+
+        var layer = svg.selectAll(".layer")
+            .data(layers)
+            .enter().append("g")
+            .attr("class", "layer")
+            .style("fill", function (d, i) {
+                return color(i);
+            });
+
+        var div = d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
+
+        layer.selectAll("rect")
+            .data(function (d) {
+                return d;
+            })
+            .enter().append("rect")
+            .attr("y", function (d) {
+                return yScale(parseDate(d.data.date));
+            })
+            .attr("x", function (d) {
+                return xScale(d[0]);
+            })
+            .attr("height", yScale.bandwidth())
+            .attr("width", function (d) {
+                return xScale(d[1]) - xScale(d[0])
+            })
+            .on('mouseover', function (d, i) {
+                d3.select(this).transition()
+                    .duration('200')
+                    .attr('opacity', '.7');
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                let num = (d[1] - d[0]).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+                div.html(num)
+                    .style("left", (d3.event.pageX + 10) + "px")
+                    .style("top", (d3.event.pageY - 15) + "px");
+
+            })
+            .on('mouseout', function (d, i) {
+                d3.select(this).transition()
+                    .duration('200')
+                    .attr('opacity', '1');
+                div.transition()
+                    .duration('200')
+                    .style("opacity", 0);
+            });
+
+        svg.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + (height + 5) + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "axis axis--y")
+            .attr("transform", "translate(0,0)")
+            .call(yAxis);
+
+        var legend = svg.selectAll('.legend-bar')
+            .data(color.domain())
+            .enter()
+            .append('g')
+            .attr('class', 'legend-bar')
+            .attr('transform', function (d, i) {
+                var height = legendRectSize + legendSpacing;
+                var offset = height * color.domain().length / 2;
+                var horz = width - 40;
+                var vert = i * height + 7;
+                return 'translate(' + horz + ',' + vert + ')';
+            });
+
+        legend.append('rect')
+            .attr('width', legendRectSize)
+            .attr('height', legendRectSize)
+            .style('fill', color)
+            .style('stroke', color);
+
+        legend.append('text')
+            .attr('x', legendRectSize + legendSpacing)
+            .attr('y', legendRectSize - legendSpacing)
+            .text(function (d) {
+                return key[d]
+            });
+
+
+    }
+
+}
+initStackedBarChart.draw({
+    data: data,
+    key: key,
+    element: 'chart'
+});
+
+/*
+data = [{
+    date: 2009,
+    wage: 7.25
+}, {
+    date: 2008,
+    wage: 6.55
+}, {
+    date: 2007,
+    wage: 5.85
+}, {
+    date: 1997,
+    wage: 5.15
+}, {
+    date: 1996,
+    wage: 4.75
+}, {
+    date: 1991,
+    wage: 4.25
+}, {
+    date: 1981,
+    wage: 3.35
+}, {
+    date: 1980,
+    wage: 3.10
+}, {
+    date: 1979,
+    wage: 2.90
+}, {
+    date: 1978,
+    wage: 2.65
+}]
+
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 40
+}
+
+var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+//making graph responsive
+default_width = 700 - margin.left - margin.right;
+default_height = 500 - margin.top - margin.bottom;
+default_ratio = default_width / default_height;
+
+// Determine current size, which determines vars
+function set_size() {
+    current_width = window.innerWidth;
+    current_height = window.innerHeight;
+    current_ratio = current_width / current_height;
+    // desktop
+    if (current_ratio > default_ratio) {
+        h = default_height;
+        w = default_width;
+        // mobile
+    } else {
+        margin.left = 40
+        w = current_width - 40;
+        h = w / default_ratio;
+    }
+    // Set new width and height based on graph dimensions
+    width = w - 50 - margin.right;
+    height = h - margin.top - margin.bottom;
+};
+set_size();
+//end responsive graph code
+
+
+// format the data
+data.forEach(function (d) {
+    parseDate = d3.timeParse("%Y");
+    d.date = parseDate(d.date);
+    d.wage = +d.wage;
+});
+//sort the data by date so the trend line makes sense
+data.sort(function (a, b) {
+    return a.date - b.date;
+});
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+// Scale the range of the data
+x.domain(d3.extent(data, function (d) {
+    return d.date;
+}));
+y.domain([0, d3.max(data, function (d) {
+    return d.wage;
+})]);
+
+// define the line
+var valueline = d3.line()
+    .x(function (d) {
+        return x(d.date);
+    })
+    .y(function (d) {
+        return y(d.wage);
+    });
+
+// append the svg object to the body of the page
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+// Add the trendline
+svg.append("path")
+    .data([data])
+    .attr("class", "line")
+    .attr("d", valueline)
+    .attr("stroke", "#32CD32")
+    .attr("stroke-width", 2)
+    .attr("fill", "#222");
+    //.attr("fill", "#FFFFFF");
+    
+
+// Add the data points
+var path = svg.selectAll("dot")
+    .data(data)
+    .enter().append("circle")
+    .attr("r", 5)
+    .attr("cx", function (d) {
+        return x(d.date);
+    })
+    .attr("cy", function (d) {
+        return y(d.wage);
+    })
+    .attr("stroke", "#32CD32")
+    .attr("stroke-width", 1.5)
+    .attr("fill", "#FFFFFF")
+
+// Add the axis
+if (width < 500) {
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).ticks(5));
+} else {
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+}
+
+svg.append("g")
+    .call(d3.axisLeft(y).tickFormat(function (d) {
+        return "$" + d3.format(".2f")(d)
+    }));
+*/
