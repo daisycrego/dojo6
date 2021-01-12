121af6cdebf07d6e36d4f71f31cd074e8082aaad
daisycrego <daisycrego@gmail.com>
2021-01-12 11:31:55 -0500

    Changing order of main calls to run the scheduler before starting the app.

--- application.py
+++ application.py
@@ -897,12 +897,7 @@ def log_data_collection(collection_type=None, listings=[]):
     db.session.add(data_collection)
     db.session.commit()
 
-# Set up weekly cron job for scraping the listings
-    scheduler = BackgroundScheduler(daemon=True)
-    scheduler.configure(timezone='est')
-    #scheduler.add_job(scrape_listings_weekly, minutes=1)
-    scheduler.add_job(scrape_listings_weekly,'cron',minute="*")
-    #scheduler.add_job(scrape_listings_weekly, /*day_of_week="0-6", hour=10, minute=32*/, minutes=1)
+
 
 # Run the app.
 if __name__ == "__main__":
@@ -910,11 +905,19 @@ if __name__ == "__main__":
     # REMOVE BEFORE DEPLOYING.
     #application.debug = True
 
+    # Set up weekly cron job for scraping the listings
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.configure(timezone='est')
+    #scheduler.add_job(scrape_listings_weekly, minutes=1)
+    scheduler.add_job(scrape_listings_weekly,'cron',minute="*")
+    #scheduler.add_job(scrape_listings_weekly, /*day_of_week="0-6", hour=10, minute=32*/, minutes=1)
+    scheduler.start()
+
     # disable the auto-reloader - otherwise cron jobs will run twice each time when deployed in DEVELOPMENT mode
     application.run(use_reloader=False)
 
     
-    scheduler.start()
+    
 
     # Shut down the scheduler when exiting the app
     atexit.register(lambda: scheduler.shutdown())
