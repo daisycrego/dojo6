c5cbf98401380ad293290ecc2e9fe53e653cfdf7
daisycrego <daisycrego@gmail.com>
2021-01-12 16:29:23 -0500

    Adding test scheduler for heroku test.

--- application.py
+++ application.py
@@ -902,7 +902,7 @@ if not application.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
     scheduler.add_job(scrape_listings_weekly, 'cron', day_of_week="fri", hour=17, minute=30)
 
     # TEST
-    scheduler.add_job(scrape_listings_weekly, 'cron', day_of_week="tue", hour=16, minute=26)
+    scheduler.add_job(scrape_listings_weekly, 'cron', day_of_week="tue", hour=16, minute=35)
 
     # Every minute
     # scheduler.add_job(scrape_listings_weekly,'cron',minute="*")
