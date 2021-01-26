3b16e3e23078e5cfa600cc57f151b828d0184ffc
daisycrego <daisycrego@gmail.com>
2021-01-26 12:31:54 -0500

    Another syntax fix

--- application.py
+++ application.py
@@ -157,11 +157,12 @@ class WebScraper:
                     #binary = FirefoxBinary(os.environ.get("FIREFOX_BINARY_PATH"))
                     CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
                     chrome_bin = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
+                    print(f"GOOGLE_CHROME_BIN: {chrome_bin")
                     options = webdriver.ChromeOptions()
                     options.binary_location = chrome_bin
-                    options.add_argument(" —-no-sandbox")
-                    options.add_argument(" —-disable-gpu")
-                    options.add_argument(" —-headless")
+                    options.add_argument("—-no-sandbox")
+                    options.add_argument("—-disable-gpu")
+                    options.add_argument("—-headless")
                     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
 
                     #options = Options()
